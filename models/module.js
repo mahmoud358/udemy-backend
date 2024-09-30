60c98ece7f15cb82f70373aca72d83a1d38e4670
mahmoud abdelhamied <mahmoudabdelhamied68@gmail.com>
2024-09-30 04:50:52 +0300

    update in module and lesson

--- models/module.js
+++ models/module.js
@@ -10,11 +10,11 @@ let moduleSchema=mongoose.Schema({
         minLength:[3,"The name must be greater than 3 letters"],
         required:true
     },
-    description:{
-        type:String,
-        minLength:[10,"The description must be greater than 10 letters"],
-        required:true
-    }
+    // description:{
+    //     type:String,
+    //     minLength:[10,"The description must be greater than 10 letters"],
+    //     required:true
+    // }
 });
 
 module.exports=mongoose.model("module",moduleSchema);
