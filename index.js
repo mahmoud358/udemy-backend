add9a84ac81edbb81bca179f0450a314c05771ac
mahmoud abdelhamied <ahmed.mamdouh9571@gmail.com>
2024-10-31 18:44:44 +0300

    update in cors

--- index.js
+++ index.js
@@ -37,19 +37,9 @@ const pusher = new Pusher({
 app.set('pusher', pusher);
 app.use(express.json());
 app.use(cors({
-  origin: '*',
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  credentials: true
+  origin: '*'
 }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://udemy-next-nu.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
+
 
 
 
