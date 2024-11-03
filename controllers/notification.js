5daf558a84b9a13319de353330f12edf33d7f986
mahmoud abdelhamied <ahmed.mamdouh9571@gmail.com>
2024-11-03 02:26:42 +0200

    update notification  controller and  model

---
+++ controllers/notification.js
@@ -0,0 +1,34 @@
+const NotificationModel= require('../models/notification')
+const APIERROR=require('../utils/apiError');
+
+const createNotification=async(req,res,next)=>{
+    let notification=req.body
+    try{
+        let result=await NotificationModel.create(notification)
+        res.status(201).json({status:"success",data:result})
+    }catch(err){
+        next(new APIERROR(404,err.message));
+    }
+}
+const getAllNotification=async(req,res,next)=>{
+    try{
+        let result=await NotificationModel.find()
+        res.status(200).json({status:"success",data:result})
+    }catch(err){
+        next(new APIERROR(404,err.message));
+    }   
+}
+const getNotificationByUesrId=async(req,res,next)=>{
+    let userId=req.id
+    try{
+        let result=await NotificationModel.find({recipient:userId})
+        if(!result){
+            next(new APIERROR(404,"no notification found"))
+        }
+        res.status(200).json({status:"success",data:result})
+    }catch(err){
+        next(new APIERROR(404,err.message));
+    }
+}
+
+module.exports={createNotification,getAllNotification,getNotificationByUesrId}
