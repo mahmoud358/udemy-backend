c4ee760e6232290c8f3618ae0eb7ed0fc478c3e3
mahmoud abdelhamied <ahmed.mamdouh9571@gmail.com>
2024-11-05 02:00:08 +0200

    add link angular dashbord in cinfigrion on cors

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ app.use(express.json());
 app.use(cors({
   origin: ['http://localhost:4200',
     'http://localhost:3000',
-    'https://udemy-next-nu.vercel.app'
+    'https://udemy-next-nu.vercel.app',
+    'https://udemy-dashboard.vercel.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
